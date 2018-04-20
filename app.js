9c22e4b3a396c8bd6b2ee2293422983587211662
liu923014086 <923014086@qq.com>
2018-04-20 13:52:00 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+App({
+  onLaunch: function () {
+    var that = this;
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              //console.info(res);
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      });
+    }
+  },
+  globalData: {
+    userInfo: null,
+    appid:'wxa2b49eec4c34e18a',
+    server: 'https://7a242db2.ngrok.io'
+  }
+});
+
